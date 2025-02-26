1c5e87a80bcf8a6ce07e00b8116c9864b64beb4d
dylan-turner25 <dylan.turner25@uga.edu>
2025-02-26 05:51:53 -0500

    adding test coverage for helper functions

--- tests/testthat/test-valid_crop.R
+++
@@ -1,7 +0,0 @@
-
-test_that("valid crop returns expected output", {
-  expect_equal(valid_crop("corn"), T)
-  expect_equal(valid_crop("not a crop"), F)
-  expect_equal(valid_crop(41), T)
-  expect_equal(valid_crop(123456789), F)
-})
