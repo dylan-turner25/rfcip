fde15c8e9b69569915cdcf556b68acdbceb1c581
dylan-turner25 <dylan.turner25@uga.edu>
2025-03-30 07:22:08 -0400

    adding internal data set for the livestock price reinsurance agreement

--- R/data.R
+++ R/data.R
@@ -26,3 +26,32 @@
 #' @source \url{https://www.rma.usda.gov/tools-reports/reinsurance-reports}
 #' 
 "nationalSRA"
+
+
+#' National Data on RMA's Livestock Price Reinsurance Agreement
+#'
+#' A data set containing all of the information published in RMA's annual
+#' Livestock Price Reinsurance Agreement reports going back to 2014.
+#' 
+#' @details
+#' The full standard reinsurance agreement can be found at \url{https://www.rma.usda.gov/policy-procedure/reinsurance-agreements}. To 
+#' view code used to generate this data set, see `./data-raw/reinsurance_reports/get_reinsurance_reports.R`  
+#' 
+#'
+#' @format  A data frame
+#' \describe{
+#'   \item{reinsurance_year}{Year of the reinsurance report}
+#'   \item{report_geography}{Geography of the report (i.e. national)}
+#'   \item{value_type}{the type of value being measured}
+#'   \item{dollars}{Nominal US Dollars corresponding to what is being measured in the value_type column} 
+#'   \item{data_release_month}{Month of the data release}
+#'   \item{data_release_year}{Year of the data release}
+#'   \item{data_release_day}{Day of the data release}
+#'   \item{data_release_date}{Date of the data release}
+#'   \item{report_type}{Type of report}
+#'   \item{footnote}{Footnote associated with the data if one was found in the downloaded file}
+#' }
+#' @usage data(nationalSRA)
+#' @source \url{https://www.rma.usda.gov/tools-reports/reinsurance-reports}
+#' 
+"nationalLPRA"
--- data-raw/reinsurance_reports/get_reinsurance_reports.R
+++ data-raw/reinsurance_reports/get_reinsurance_reports.R
@@ -11,11 +11,12 @@ library(purrr)
 #' @param year the year of the reinsurance report
 #' @param geography a character string specifying the geography of the report. Options are "national" or "state"
 #' @param type a character string specifying the type of reinsurance report. Options are "standard" or "livestock"
+#' @param livestock_download_links a list of download links for livestock reinsurance reports. This is only required if type = "livestock"
 #'
 #' @returns a rds file saved in the data-raw/reinsurance_reports directory
 #'
 #' @source Data is downloaded directly from RMA's reinsurance reports app: \url{https://public-rma.fpac.usda.gov/apps/ReinsuranceReports/}
-download_reinsurance_reports <- function(year,geography,type = "standard") {
+download_reinsurance_reports <- function(year,geography,type = "standard",livestock_download_links = NULL){ 
     
   # type check geography
   if(tolower(geography) == "national"){
@@ -24,6 +25,14 @@ download_reinsurance_reports <- function(year,geography,type = "standard") {
     geography = "StateFund"
   } 
   
+  # check for livestock download links if type is livestock
+  if(type == "livestock"){
+    if(is.null(livestock_download_links)){
+      stop("Livestock download links must be provided for livestock reinsurance reports.")
+    }
+  }
+  
+  # check for invalid combinations
   if(geography == "StateFund" & type == "livestock"){
     stop("Livestock reinsurance reports are not available at the state level. Please use 'national' for livestock reports.")
   }
@@ -111,7 +120,98 @@ download_reinsurance_reports <- function(year,geography,type = "standard") {
   }
   
   if(type == "livestock"){
-    # get livestock reports
+    tryCatch({ 
+      print(year)
+      
+      # set temporary directory
+      file <-tempfile(fileext = ".xls")
+      
+      # download the file from RMA's website to a temporary file
+      download.file(url=livestock_download_links[[as.character(year)]], 
+                    destfile = file,
+                    method = "curl", mode = "wb")
+      
+      
+      # load the data from the temporary file
+      data  <- as.data.frame(xlsx::read.xlsx(file,sheetIndex = 1)) 
+      
+      # identify which cell in data contains "As of"
+      as_of_row <- NULL
+      i = 0
+      while(length(as_of_row) == 0){
+        i = i + 1
+        as_of_row <- which(grepl("As of",data[,i]))
+        if(i > 20){
+          warning("No data date found")
+        }
+      }
+      data_date <- gsub("As of ","",data[as_of_row,i])
+      
+      
+      # locate first content line
+      first_content_line <- NULL
+      i = 0
+      while(length(first_content_line) == 0){
+        i = i + 1
+        first_content_line <- which(grepl("Gross",data[,i]))
+        if(i > 20){
+          warning("No content line found")
+          break
+        }
+      }
+      
+      # locate footnote line
+      footnote_row <- NULL
+      i = 0
+      while(length(footnote_row) == 0){
+        i = i + 1
+        footnote_row <- which(grepl("Footnote",data[,i]))
+        if(i > 20){
+          warning("No footnote found")
+        }
+      }
+      
+      # extract footnote
+      footnote <- paste0("Footnote: ",data[footnote_row+1,i])
+      
+      # remove any rows at or after the footnote row
+      data <- data[1:(footnote_row-1),]
+
+      
+      # get column names
+      colnames(data) <- data[first_content_line,]
+      
+      # remove the header
+      data <- data[(first_content_line + 1):nrow(data),] 
+      
+      # remove columns that are all NA values
+      data <- data[, colSums(is.na(data)) < nrow(data)] 
+      
+      # remove rows that are all NA values
+      data <- data[rowSums(is.na(data)) < ncol(data),]
+      
+      # clean column names using janitor::clean_names
+      data <- janitor::clean_names(data)
+      
+      # add some additional columns
+      data$reinsurance_year <- year
+      data$report_geography  <- geography
+      data$data_date <- data_date
+      data$footnote <- footnote
+      
+      # pivot longer
+        data <- data %>% 
+          tidyr::pivot_longer(cols = -c(reinsurance_year,report_geography,data_date,footnote), 
+                              names_to = "value_type", 
+                              values_to = "dollars")
+        
+   
+      # save data as a rds file
+      file_path <- paste0("./data-raw/reinsurance_reports/",type,"/", 
+                          geography,"_", year, ".rds")
+      saveRDS(data, file_path)
+      
+    }, error=function(e){})
   }
 }
 
@@ -123,7 +223,7 @@ parse_data_release_date <- function(df, date_col = "data_date") {
   }
   
   # Extract month and year
-  df$data_release_month <- substr(df[[date_col]], 1, 2)
+  df$data_release_month <- gsub("/","",substr(df[[date_col]], 1, 2))
   df$data_release_year <- substr(df[[date_col]],
                                  nchar(df[[date_col]]) - 3, 
                                  nchar(df[[date_col]]))
@@ -182,6 +282,27 @@ mapply(FUN = download_reinsurance_reports,
        year = 1998:as.numeric(format(Sys.Date(),"%Y")), 
        MoreArgs = list(geography = "state", type = "standard"))
 
+# download and save national reinsurance reports for livestock reinsurance agreement
+
+# links for the livestock reinsurance report excel files. Must be periodically updated if new data is available (typically once per year).
+# Automatically web scraping the most recent links would be a good feature upgrade later. 
+livestock_links <- list("2014" = "https://www.rma.usda.gov/sites/default/files/2024-06/Livestock%20Reinsurance%20Report_2014-xls.xls",
+                        "2015" = "https://www.rma.usda.gov/sites/default/files/2024-06/Livestock%20Reinsurance%20Report_2015-xls.xls",
+                        "2016" = "https://www.rma.usda.gov/sites/default/files/2024-06/Livestock%20Reinsurance%20Report_2016-xls.xls",
+                        "2017" = "https://www.rma.usda.gov/sites/default/files/2024-06/Livestock%20Reinsurance%20Report_2017-xls.xls",
+                        "2018" = "https://www.rma.usda.gov/sites/default/files/2024-06/Livestock%20Reinsurance%20Report_2018-xls.xlsx",
+                        "2019" = "https://www.rma.usda.gov/sites/default/files/2024-06/Livestock%20Reinsurance%20Report_2019-xls.xls",
+                        "2020" = "https://www.rma.usda.gov/sites/default/files/2024-08/Livestock%20Reinsurance%20Report_2020.xls",
+                        "2021" = "https://www.rma.usda.gov/sites/default/files/2025-03/Livestock%20Reinsurance%20Report_2021.xls",
+                        "2022" = "https://www.rma.usda.gov/sites/default/files/2025-02/Livestock%20Reinsurance%20Report_2022.xls",
+                        "2023" = "https://www.rma.usda.gov/sites/default/files/2025-02/Livestock%20Reinsurance%20Report_2023.xls")
+
+# apply the download function to the livestock links
+mapply(FUN = download_reinsurance_reports, 
+       year = as.numeric(names(livestock_links)), 
+       MoreArgs = list(geography = "national", 
+                       type = "livestock", 
+                       livestock_download_links = livestock_links))
 
 
 # clean the downloaded national reinsurance reports for the standard reinsurance agreement ---------------
@@ -207,5 +328,28 @@ national_reinsurance_reports$report_type <- "Standard Resinsurance Agreement"
 # export as a data set
 nationalSRA = national_reinsurance_reports
 usethis::use_data(nationalSRA, overwrite = TRUE)
+
+
+# clean the downloaded state reinsurance reports for the standard reinsurance agreement ---------------
+
+# clean the downloaded national reinsurance reports for the livestock reinsurance agreement -----------
+livestock_reinsurance_reports <- list.files(path = "./data-raw/reinsurance_reports/livestock", 
+                                           pattern = "*.rds", full.names = TRUE) %>% 
+  purrr::map(readRDS) %>%
+  dplyr::bind_rows() 
+
+
+# clean date using parse_data_release_date function
+livestock_reinsurance_reports<- parse_data_release_date(livestock_reinsurance_reports) %>%
+  select(-data_date)
+
+# add a column indicating the type of reinsurance report
+livestock_reinsurance_reports$report_type <- "Livestock Price Reinsurance Agreement"
+
+
+# export as a data set
+nationalLPRA = livestock_reinsurance_reports
+usethis::use_data(nationalLPRA, overwrite = TRUE)
+
 
 
